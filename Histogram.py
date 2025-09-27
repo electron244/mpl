2bcb3eded857c1e2bed5a7b6513ab1df5b7af652
Sidharth Sinh <zalasidharth244@gmail.com>
2025-09-27 16:56:12 +0530

    Histogram Updaeted

--- Histogram.py
+++ Histogram.py
@@ -4,7 +4,5 @@ marks = [34,53,55,34,23,67,87,76,87,65,89,90,99,54,33,44,12,6,76,54,43,79]
 plt.hist(marks,bins=6,color='orange',edgecolor='white')
 plt.xlabel('Marks Range')
 plt.ylabel('Number of Students')
-
 plt.title('Student Marks Distribution')
-
 plt.show()
