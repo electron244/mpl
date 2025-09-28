baacf163f7bfef4a75c2e08b3e5ee6cce77553ac
Sidharth Sinh <zalasidharth244@gmail.com>
2025-09-28 23:28:52 +0530

    Top 10 Movies Graph

--- Project_2/main.py
+++ Project_2/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('./movies.csv')
+print(df.head())
 
 # 1. Most Popular Genre (by count of movies)
 popular_genre = df["genre"].value_counts()
@@ -13,3 +14,18 @@ plt.ylabel("Count")
 plt.title("Movie Genre & Count")
 plt.show()
 
+# 2. Average Rating by Genre
+avg_rating_genre = df.groupby('genre')['rating'].mean()
+plt.bar(avg_rating_genre.index,avg_rating_genre.values,color='orange')
+plt.xlabel("Genre")
+plt.ylabel("Avg. Rating")
+plt.title("Average Rating")
+plt.show()
+
+# 3. Top 10 Highest Rated Movies
+top_movies = df.sort_values(by='rating',ascending=False).head(10)
+plt.barh(top_movies['title'],top_movies["rating"],color='orange')
+plt.xlabel("IMDb Rating")
+plt.ylabel("Movies Name")
+plt.title("Top 10 Highest Rated Movies")
+plt.show()
