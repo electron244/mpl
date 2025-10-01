2e3fdefe12447060751a88dafc570a006a0a7d7e
Sidharth Sinh <zalasidharth244@gmail.com>
2025-10-01 16:44:26 +0530

    Project 3 done

--- Project_3/main.py
+++ Project_3/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.ndimage import label
+
+# 3. Stock Price Analysis Project 📈
 
 df = pd.read_csv("./stock.csv")
 
@@ -14,3 +15,14 @@ plt.ylabel("Close Price")
 plt.title("Stock Closing Price Trend")
 plt.show()
 
+# Moving Averages
+df['7_days_average'] = df['close'].rolling(window=7).mean()
+df['30_days_average'] = df['close'].rolling(window=30).mean()
+
+plt.plot(df['date'],df['close'],alpha=0.5)
+plt.plot(df['date'],df['7_days_average'],color='orange')
+plt.plot(df['date'],df['30_days_average'],color='red')
+plt.title("Stock Price with Moving Average")
+plt.xlabel("Date")
+plt.ylabel("Price")
+plt.show()
