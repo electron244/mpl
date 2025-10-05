b162278714c4d81a3152e2debea124505ef6403b
Sidharth Sinh <zalasidharth244@gmail.com>
2025-10-05 10:47:25 +0530

    scatter code added

--- Netflix_data_analysis/main.py
+++ Netflix_data_analysis/main.py
@@ -22,3 +22,22 @@ plt.figure(figsize=(8,6))
 plt.pie(rating_counts,labels=rating_counts.index,autopct='%1.1f%%',startangle=90)
 plt.title("Percentage of Content Rating")
 plt.show()
+
+# Duration histograph of Movies
+movie_df = data[data['type']=='Movie'].copy()
+movie_df['duration'] = movie_df['duration'].str.replace('min','').astype(int)
+plt.hist(movie_df['duration'],bins=30,color='purple',edgecolor='black')
+plt.title("Distribution of Movies duration")
+plt.xlabel('Duration (in min)')
+plt.ylabel('Number of Movies')
+plt.show()
+
+
+release_counts = data['release_year'].value_counts().sort_index()
+plt.figure(figsize=(10,6))
+plt.scatter(release_counts.index,release_counts.values,color='red')
+plt.title("Release year vs Number of Shows")
+plt.xlabel("Release Year")
+plt.ylabel("Number of Shows")
+plt.grid(axis='y')
+plt.show()
